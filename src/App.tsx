9e4a37e5dd1af61b62678b708a7b8a67f5e62cf4
Oskar Compains <14krokodilo14@gmail.com>
2026-09-15 22:33:22 +0200

    Enable secure authentication provider

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,11 @@
  * @license
  * SPDX-License-Identifier: Apache-2.0
  */
-
 import React from 'react';
 import { AppProvider } from './context/AppContext';
+import { SecureAuthProvider } from './context/SecureAuthContext';
 import { Layout } from './components/layout/Layout';
 
-export default function App() {
-  return (
-    <AppProvider>
-      <Layout />
-    </AppProvider>
-  );
+export default function App(){
+ return <SecureAuthProvider><AppProvider><Layout /></AppProvider></SecureAuthProvider>;
 }
